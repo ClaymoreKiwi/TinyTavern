ef018a20564b79f844e3284272818389b4646292
Bryce Tingle <tinglebryce@gmail.com>
2023-05-14 22:56:11 +0100

    1

---
+++ Proj/TinyTavern/Intermediate/Build/Win64/UnrealEditor/Inc/TinyTavern/UHT/TinyTavernGameModeBase.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "TinyTavernGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TINYTAVERN_TinyTavernGameModeBase_generated_h
+#error "TinyTavernGameModeBase.generated.h already included, missing '#pragma once' in TinyTavernGameModeBase.h"
+#endif
+#define TINYTAVERN_TinyTavernGameModeBase_generated_h
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_SPARSE_DATA
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_RPC_WRAPPERS
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_ACCESSORS
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATinyTavernGameModeBase(); \
+	friend struct Z_Construct_UClass_ATinyTavernGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATinyTavernGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TinyTavern"), NO_API) \
+	DECLARE_SERIALIZER(ATinyTavernGameModeBase)
+
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATinyTavernGameModeBase(); \
+	friend struct Z_Construct_UClass_ATinyTavernGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATinyTavernGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TinyTavern"), NO_API) \
+	DECLARE_SERIALIZER(ATinyTavernGameModeBase)
+
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATinyTavernGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATinyTavernGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATinyTavernGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATinyTavernGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATinyTavernGameModeBase(ATinyTavernGameModeBase&&); \
+	NO_API ATinyTavernGameModeBase(const ATinyTavernGameModeBase&); \
+public: \
+	NO_API virtual ~ATinyTavernGameModeBase();
+
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATinyTavernGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATinyTavernGameModeBase(ATinyTavernGameModeBase&&); \
+	NO_API ATinyTavernGameModeBase(const ATinyTavernGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATinyTavernGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATinyTavernGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATinyTavernGameModeBase) \
+	NO_API virtual ~ATinyTavernGameModeBase();
+
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_12_PROLOG
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_SPARSE_DATA \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_RPC_WRAPPERS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_ACCESSORS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_INCLASS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_SPARSE_DATA \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_ACCESSORS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TINYTAVERN_API UClass* StaticClass<class ATinyTavernGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_TinyTavern_Source_TinyTavern_TinyTavernGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
